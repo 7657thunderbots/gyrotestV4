1f47f54dfdeb676701c2bd312bd6e6f5e45bf8a1
7657thunderbots <123423766+7657thunderbots@users.noreply.github.com>
2023-01-31 16:22:11 -0600

    go on the ramp with motor encoder data

--- gyroV4/src/main/java/frc/robot/Robot.java
+++ gyroV4/src/main/java/frc/robot/Robot.java
@@ -27,17 +27,19 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.util.datalog.BooleanLogEntry;
 import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.DataLogEntry;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.util.datalog.StringLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 public class Robot extends TimedRobot {
   PWMVictorSPX m_left, m_right;
   Joystick m_controller;
   ADIS16470_IMU m_gyro;
   int timer;
-  SendableChooser<Double> m_chooser;
-  
+
 
   //mag shooter
 	private CANSparkMax intake;
@@ -62,13 +64,12 @@ public class Robot extends TimedRobot {
 	private XboxController controller2;
 
   private final Timer m_timer = new Timer();
-  private final Timer m_timer2 = new Timer();
-  private static boolean OpenFlag = false;
-	private double wormStart = 0.0;
+  private static final String kDefaultSpeed = "Demo";
+	private static final String kCompetitionSpeed = "Competition";
+	private String speed_selected;
+	private final SendableChooser<String> speed_chooser = new SendableChooser<>();
 
-  static final double WORMSPEED=0.25;
-	static final double WORMTIME=6.7;
-
+  DoubleLogEntry myDoubleLog;
   private boolean autolevelingage = false;
   private boolean chargelevel = false;
   
@@ -79,21 +80,21 @@ public class Robot extends TimedRobot {
   private double anglelast=0.0;
   private double newangle=0.0;
   private final PowerDistribution m_pdp = new PowerDistribution();
+
  
+  
   @Override
   public void robotInit() {
+   
     m_right = new PWMVictorSPX(0);
     m_left = new PWMVictorSPX(1);
     m_controller = new Joystick(0);
     m_gyro = new ADIS16470_IMU();
-    m_chooser = new SendableChooser<>();
-
-    m_right.setInverted(true);
-    SmartDashboard.putData(m_chooser);
+    
 
 		speedMult = .5;
-
-    DataLogManager.start();
+    DataLog log = DataLogManager.getLog();
+    myDoubleLog = new DoubleLogEntry(log, "gyro");
     //cams
 		CameraServer.startAutomaticCapture(0);
 		CameraServer.startAutomaticCapture(1);
@@ -135,7 +136,10 @@ public class Robot extends TimedRobot {
 		left = new Joystick(0);
 		right = new Joystick(1);
 		controller2 = new XboxController(2);
-	  }
+	  speed_chooser.setDefaultOption("DemoSpeed", kDefaultSpeed);
+		speed_chooser.addOption("Competition Speed", kCompetitionSpeed);
+		SmartDashboard.putData("Speed choices", speed_chooser);
+  }
   
 
   
@@ -144,8 +148,7 @@ public class Robot extends TimedRobot {
     m_gyro.reset();
     m_timer.reset();
 		m_timer.start();
-  m_timer2.reset();
-  m_timer2.start();
+    DataLogManager.start();
   }
 
 
@@ -153,45 +156,52 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-    
-    if (m_timer.get() < 5 && m_gyro.getYComplementaryAngle()>-15)
-    { directionL=.45;
-    directionR=.45;
+   newangle=m_gyro.getYComplementaryAngle();
+    SmartDashboard.putNumber("timer", m_timer.get());
+    if (m_timer.get() < 3 && leftParent.getSelectedSensorPosition()<120830 && m_gyro.getYComplementaryAngle()<3) { 
+      directionL=.55;
+      directionR=.55;
+    } else {
+
+      setpoint = m_gyro.getYComplementaryAngle()*-.0275 - (newangle - anglelast)*.025;
+      if (setpoint > 0.5){
+        setpoint=.5;
+      }
+      if (setpoint<-.5){
+       setpoint=-.5;
+     }
     }
-   else if (m_timer.get()>=3)
-   {
-    setpoint = m_gyro.getYComplementaryAngle()*-.0275 - (newangle - anglelast)*.02;
-    if (setpoint > 0.55){
-      setpoint=.55;}
-      if (setpoint<-.55){
-        setpoint=-.55;}
-      
-    directionL=setpoint;
-    directionR=setpoint;
+    
+    
+      directionL=setpoint;
+      directionR=setpoint;
    
 
-        SmartDashboard.putNumber("robotangle", m_gyro.getYComplementaryAngle());
-        SmartDashboard.putNumber("turnangle", m_gyro.getAngle());
+    SmartDashboard.putNumber("robotangle", m_gyro.getYComplementaryAngle());
+    SmartDashboard.putNumber("turnangle", m_gyro.getAngle());
         
-    }
+    
     if (m_gyro.getAngle()>3){
-      turnerror = .1;}
-      else if (m_gyro.getAngle()<3 && m_gyro.getAngle() >-3){
+      turnerror = .1;
+    }else if (m_gyro.getAngle()<3 && m_gyro.getAngle() >-3){
         turnerror =0;
-      }
-      else if (m_gyro.getAngle()<-3){
-        turnerror =-.1;}
+    }else if (m_gyro.getAngle()<-3){
+        turnerror =-.1;
+    }
+    
     tankDrive.tankDrive (turnerror+directionL,-turnerror+directionR);
-
+    
+    myDoubleLog.append(m_gyro.getYComplementaryAngle());
+    
     SmartDashboard.putData("PDP", m_pdp);
-    SmartDashboard.putNumber("tilt angle", m_gyro.getYComplementaryAngle());
-    SmartDashboard.putNumber("voltage",m_pdp.getVoltage());
-   SmartDashboard.putNumber("PDP current", m_pdp.getTotalCurrent());
-    SmartDashboard.putNumber("Total energy", m_pdp.getTotalEnergy());
-    SmartDashboard.putNumber("Total power", m_pdp.getTotalPower());
+     SmartDashboard.putNumber("tilt angle", m_gyro.getYComplementaryAngle());
+     SmartDashboard.putNumber("voltage",m_pdp.getVoltage());
+     SmartDashboard.putNumber("PDP current", m_pdp.getTotalCurrent());
+     SmartDashboard.putNumber("Total energy", m_pdp.getTotalEnergy());
+     SmartDashboard.putNumber("Total power", m_pdp.getTotalPower());
     SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(9));
-    SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(8));
-    SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(10));
+     SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(8));
+     SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(10));
     SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(11));
     SmartDashboard.putNumber("turn angle",m_gyro.getAngle());
     SmartDashboard.putNumber("Constant speed left",directionL);
@@ -204,28 +214,51 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("filtered x acceleration Angle",m_gyro.getXFilteredAccelAngle() );
     SmartDashboard.putNumber("filtered y acceleration Angle",m_gyro.getYFilteredAccelAngle() );
     SmartDashboard.putNumber("X Complimentary angle",m_gyro.getXComplementaryAngle() );
-  SmartDashboard.putNumber("setpoint", setpoint);
-  SmartDashboard.putNumber("newangle", newangle);
-  SmartDashboard.putNumber("oldangle", anglelast);
-  SmartDashboard.putNumber("timer", m_timer.get());
+    SmartDashboard.putNumber("setpoint", setpoint);
+    SmartDashboard.putNumber("newangle", newangle);
     anglelast=m_gyro.getYComplementaryAngle();
+    anglelast=newangle;
+    SmartDashboard.putNumber("oldangle", anglelast);
+    SmartDashboard.putNumber("timer", m_timer.get());
+    myDoubleLog.append(directionL);
+    myDoubleLog.append(directionR);
+    myDoubleLog.append(m_pdp.getVoltage());
+    myDoubleLog.append(setpoint);
+    System.out.println(leftParent.getSelectedSensorPosition()); // prints the position of the selected sensor
+    System.out.println(leftParent.getSelectedSensorVelocity()); // prints the velocity recorded by the selected sensor
+    System.out.println(leftParent.getMotorOutputPercent()); // prints the percent output of the motor (0.5)
+    System.out.println(leftParent.getStatorCurrent()); // prints the output current of the motor
+    SmartDashboard.putNumber("position", leftParent.getSelectedSensorPosition());
+    SmartDashboard.putNumber("velocity", leftParent.getSelectedSensorVelocity());
+    SmartDashboard.putNumber("output percent", leftParent.getMotorOutputPercent());
+    SmartDashboard.putNumber("stator current", leftParent.getStatorCurrent());
   }
 
 
+
+
       
     
     
       
 
-
+@Override
+public void teleopInit(){
+  DataLogManager.start();
+}
   
     
 
   @Override
   public void teleopPeriodic() {
+    speed_selected = speed_chooser.getSelected();
+		SmartDashboard.putString("Speed Chosen", speed_selected);
+
     m_right.set(-m_controller.getRawAxis(3));
     m_left.set(-m_controller.getRawAxis(1));
     System.out.println(m_gyro.getYComplementaryAngle());
+    tankDrive.tankDrive(right.getY() * speedMult, left.getY() * speedMult);
+			tankDrive.feedWatchdog(); 
     if (autolevelingage){
 			SmartDashboard.putString("autolevelingage", "true");
 		}else {
@@ -236,16 +269,16 @@ public class Robot extends TimedRobot {
       }else {
         SmartDashboard.putString("chargelevel", "false");
         }
-            SmartDashboard.putData("PDP", m_pdp);
-            SmartDashboard.putNumber("tilt angle", m_gyro.getYComplementaryAngle());
-            SmartDashboard.putNumber("voltage",m_pdp.getVoltage());
-           SmartDashboard.putNumber("PDP current", m_pdp.getTotalCurrent());
-            SmartDashboard.putNumber("Total energy", m_pdp.getTotalEnergy());
-            SmartDashboard.putNumber("Total power", m_pdp.getTotalPower());
-            SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(9));
-            SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(8));
-            SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(10));
-            SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(11));
+            //SmartDashboard.putData("PDP", m_pdp);
+           // SmartDashboard.putNumber("tilt angle", m_gyro.getYComplementaryAngle());
+            //SmartDashboard.putNumber("voltage",m_pdp.getVoltage());
+          //  SmartDashboard.putNumber("PDP current", m_pdp.getTotalCurrent());
+          //   SmartDashboard.putNumber("Total energy", m_pdp.getTotalEnergy());
+          //   SmartDashboard.putNumber("Total power", m_pdp.getTotalPower());
+          //   SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(9));
+          //   SmartDashboard.putNumber("Current used by: drivemotor right", m_pdp.getCurrent(8));
+          //   SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(10));
+          //   SmartDashboard.putNumber("Current used by: drivemotor left", m_pdp.getCurrent(11));
             SmartDashboard.putNumber("turn angle",m_gyro.getAngle());
             SmartDashboard.putNumber("Constant speed left",directionL);
             SmartDashboard.putNumber("constant speed right", directionR);
@@ -257,9 +290,14 @@ public class Robot extends TimedRobot {
             SmartDashboard.putNumber("filtered x acceleration Angle",m_gyro.getXFilteredAccelAngle() );
             SmartDashboard.putNumber("filtered y acceleration Angle",m_gyro.getYFilteredAccelAngle() );
             SmartDashboard.putNumber("X Complimentary angle",m_gyro.getXComplementaryAngle() );
-            SmartDashboard.putNumber("setpoint", setpoint);
-            SmartDashboard.putNumber("newangle", newangle);
-            SmartDashboard.putNumber("oldangle", anglelast);
+            //SmartDashboard.putNumber("setpoint", setpoint);
+            //SmartDashboard.putNumber("newangle", newangle);
+            //SmartDashboard.putNumber("oldangle", anglelast);
+            myDoubleLog.append(m_gyro.getYComplementaryAngle());
+            SmartDashboard.putNumber("position", leftParent.getSelectedSensorPosition());
+            SmartDashboard.putNumber("velocity", leftParent.getSelectedSensorVelocity());
+            SmartDashboard.putNumber("output percent", leftParent.getMotorOutputPercent());
+            SmartDashboard.putNumber("stator current", leftParent.getStatorCurrent());
           }
           
             
